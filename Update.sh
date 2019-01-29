3149f3e3d28d26fe23b60688ac42a457c63f95b5
ewinger581 <44706518+ewinger581@users.noreply.github.com>
2019-01-28 18:07:05 -0600

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,5 @@
+##Set daily update
+sed -i -e 's/APT::Periodic::Update-Package-Lists.*\+/APT::Periodic::Update-Package-Lists "1";/' /etc/apt/apt.conf.d/10periodic
+sed -i -e 's/APT::Periodic::Download-Upgradeable-Packages.*\+/APT::Periodic::Download-Upgradeable-Packages "0";/' /etc/apt/apt.conf.d/10periodic
+apt get update
+apt get upgrade
