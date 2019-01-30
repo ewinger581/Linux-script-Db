087ec265577dfa56700dc3b58fa8be0052ff9ae6
ewinger581 <44706518+ewinger581@users.noreply.github.com>
2019-01-29 18:17:10 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,6 +1,6 @@
 ##Set daily update
 sed -i -e 's/APT::Periodic::Update-Package-Lists.*\+/APT::Periodic::Update-Package-Lists "1";/' /etc/apt/apt.conf.d/10periodic
 sed -i -e 's/APT::Periodic::Download-Upgradeable-Packages.*\+/APT::Periodic::Download-Upgradeable-Packages "0";/' /etc/apt/apt.conf.d/10periodic
-apt install update
-apt install upgrade
+apt-get update
+apt-get upgrade
 apt install firefox -y
